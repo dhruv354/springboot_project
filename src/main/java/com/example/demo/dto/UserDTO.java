50e3d87597cbd2d017108bec37ca9c3b9c8d5eea
NC24497 Dhruv Singhal <nc24497dhruvsinghal@Ninja-ITs-MacBook-Air.local>
2023-04-19 11:32:02 +0530

    create user dto and response list dto for accessing github users

---
+++ src/main/java/com/example/demo/dto/UserDTO.java
@@ -0,0 +1,79 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.Address;
+
+import javax.validation.constraints.Min;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+
+    private HairDTO hair;
+
+    private String gender;
+
+    @Min(0)
+    private int age;
+
+    private String phone;
+
+    private Address address;
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public HairDTO getHair() {
+        return hair;
+    }
+
+    public void setHair(HairDTO hair) {
+        this.hair = hair;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
