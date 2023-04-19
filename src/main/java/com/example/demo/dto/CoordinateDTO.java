d98dd896a11cffe27a4f3bb990c5fb32eddc0b94
NC24497 Dhruv Singhal <nc24497dhruvsinghal@Ninja-ITs-MacBook-Air.local>
2023-04-19 11:32:35 +0530

    create coordinate  dto to access latitudes and longitudes

---
+++ src/main/java/com/example/demo/dto/CoordinateDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.dto;
+
+public class CoordinateDTO {
+    private float lat;
+
+    private float lon;
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLon() {
+        return lon;
+    }
+
+    public void setLon(float lon) {
+        this.lon = lon;
+    }
+}
