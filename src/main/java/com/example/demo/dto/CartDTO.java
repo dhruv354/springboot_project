102654fceea4efa642433ee4d6edaf6c82f5f56a
NC24497 Dhruv Singhal <nc24497dhruvsinghal@Ninja-ITs-MacBook-Air.local>
2023-04-19 11:31:07 +0530

    added Cart Dto

---
+++ src/main/java/com/example/demo/dto/CartDTO.java
@@ -0,0 +1,39 @@
+package com.example.demo.dto;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class CartDTO {
+
+    private int user_id;
+
+    private int quantity;
+
+
+    private int product_id;
+
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+}
